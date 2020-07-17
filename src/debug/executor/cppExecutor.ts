8aac305fc3d6f41838343291217315a1af4ce485
texot <tete1030@gmail.com>
2020-07-17 19:27:16 +0800

    Fix previous fault

--- src/debug/executor/cppExecutor.ts
+++ src/debug/executor/cppExecutor.ts
@@ -267,7 +267,7 @@ using namespace std;
             return;
         }
 
-        // debugConfig.stopAtEntry = true;
+        debugConfig.stopAtEntry = false;
         // debugConfig.logging = { engineLogging: true, trace: true, traceResponse: true };
         debugConfig.MIMode = vscode.workspace.getConfiguration('debug-leetcode').get('MIMode');
         const miDebuggerPath: string =
